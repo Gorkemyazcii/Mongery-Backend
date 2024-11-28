f630ac6cec4c7a64a82e8b8913ce4807ce8cab4b
Gorkemyazcii <gkrem33@gmail.com>
2024-11-28 21:49:51 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use(cors()); // CORS middleware'ini kullanın
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda çalışıyor`);
