a21156ffe107d19b9ecf5418a38a911fb6b0afd4
Görkem <123131846+Gorkemyazcii@users.noreply.github.com>
2024-11-28 22:23:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const router = require("./routes/router");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(cors());
+app.use("/api", router);
 
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda çalışıyor`);
+  console.log(Sunucu ${port} portunda çalışıyor);
 });
