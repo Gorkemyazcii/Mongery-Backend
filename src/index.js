bf84510d08066a423546b56b166e2744c0747de9
Gorkemyazcii <gkrem33@gmail.com>
2024-11-28 21:49:14 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(cors()); // CORS middleware'ini kullanın
 
-// Diğer route'larınız burada
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda çalışıyor`);
 });
